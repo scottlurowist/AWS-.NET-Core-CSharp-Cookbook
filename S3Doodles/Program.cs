26656e796cff972072ce54b7e67a1c6f24c4db38
Scott Lurowist <scottlurowist@gmail.com>
2019-07-05 12:41:47 -0400

    FINISHED S3 AND NOW STARTING LamdaDoodles/LamdaDoodles
    I have the S3 project finished, though I may add more later.
    
    I started an empty Lamda project. Now I need to learn Lamda!

--- S3Doodles/Program.cs
+++ S3Doodles/Program.cs
@@ -1,46 +1,90 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using System.Threading;
+using System.Threading.Tasks;
 using Amazon;
-using Amazon.Runtime;
 using Amazon.S3;
+using Amazon.S3.Model;
 
 namespace S3Doodles
 {
     class Program
     {
-        static void Main(string[] args)
+        private const string BucketName = "scottlurowist";
+        private const string ImageName = "Tiger-Sambar-09.03.2013-10.40PM.JPG";
+
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello To S3!");
+            Console.WriteLine("Welcome To S3!");
 
-            AWSCredentials credsBase = new BasicAWSCredentials("AKIAUO6ZESUM4FSFKYGQ",
-                "L0vOC02MkwGXLIlRqzD8YdSg6sg16dDEyMgujsgx");
-            
-            IAmazonS3 client = new AmazonS3Client(credsBase, RegionEndpoint.APNortheast1);
-            CancellationToken token = new CancellationToken();
+            using (IAmazonS3 client = new AmazonS3Client(RegionEndpoint.USEast1))
+            {
+                CancellationTokenSource source = new CancellationTokenSource();
+                CancellationToken token = source.Token;
 
-            var foo = client.ListBucketsAsync(token);
+                await ListAllBucketsAsync(client, token);
+                await ListContentsOfAwsCookbookAsync(client, token);
+                await UploadImageAsync(client, token);
+                await DownloadImageToDesktopAsync(client, token);
+            }
+        }
 
-            
+        private static async Task ListAllBucketsAsync(IAmazonS3 client, CancellationToken token)
+        {
+            ListBucketsResponse response = await client.ListBucketsAsync(token);
 
-            foreach (var bucket in foo.Result.Buckets)
+            foreach (var bucket in response.Buckets)
             {
                 Console.WriteLine(bucket.BucketName);
-
-                CancellationToken newToken = new CancellationToken();
-
-                var bar = client.ListObjectsAsync(bucket.BucketName, newToken);
-
-                var foobar = bar.Result.S3Objects;
-
-                //foreach (var someObj in bar.Result.S3Objects)
-                //{
-                //    Console.WriteLine($"\t\t\t{someObj.Key}");
-                //}
             }
+        }
 
-            client.Dispose();
+        private static async Task ListContentsOfAwsCookbookAsync(IAmazonS3 client, CancellationToken token)
+        {
+            ListObjectsResponse response =
+                await client.ListObjectsAsync(BucketName, "AWS Cookbook", token);
 
-            Console.WriteLine();
+            Console.WriteLine($"\nListing the contents of prefix 'AWS Cookbook'\n");
+
+            foreach (var resultS3Object in response.S3Objects)
+            {
+                Console.WriteLine($"   {resultS3Object.Key}");
+            }
+        }
+
+        private static async Task UploadImageAsync(IAmazonS3 client, CancellationToken token)
+        {
+            PutObjectRequest request = new PutObjectRequest()
+            {
+                BucketName = BucketName,
+                FilePath = ImageName,
+                Key = ImageName,
+                StorageClass = S3StorageClass.OneZoneInfrequentAccess
+            };
+
+            PutObjectResponse response = await client.PutObjectAsync(request, token);
+
+            if (response.HttpStatusCode == HttpStatusCode.OK)
+            {
+                Console.WriteLine($"\n\nPhoto {ImageName} was successfully uploaded to bucket 'scottlurowist'");
+            }
+        }
+
+
+        private static async Task DownloadImageToDesktopAsync(IAmazonS3 client, CancellationToken token)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            string filePath = Path.Combine(desktopPath, ImageName);
+
+            GetObjectResponse getObjResponse =
+                await client.GetObjectAsync(BucketName, ImageName, token);
+
+            await getObjResponse.WriteResponseStreamToFileAsync(filePath,
+                false, token);
+
+            Console.WriteLine($"Image {ImageName} was successfully downloaded to {desktopPath}.");
         }
     }
 }
