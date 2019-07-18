2974f68058c466700e641b1693ad7efe68f58afb
Scott Lurowist <scottlurowist@gmail.com>
2019-07-18 14:57:36 -0400

    I RENAMED SOME THINGS

---
+++ DynamoDBCookbook/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
+
+namespace DynamoDBCookbook
+{
+    class Program
+    {
+        private const string MyTableName = "SomeFilmsThatILike";
+        
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Welcome To DynamoDB!\n\n");
+
+            CancellationTokenSource source = new CancellationTokenSource();
+            CancellationToken token = source.Token;
+
+            using (IAmazonDynamoDB client = new AmazonDynamoDBClient(RegionEndpoint.USEast1))
+            {
+                await CreateTable(client, token);
+                await DeleteTable(client, token);
+            }
+        }
+
+        private static async Task CreateTable(IAmazonDynamoDB client, CancellationToken token)
+        {
+            CreateTableRequest request = new CreateTableRequest()
+            {
+                TableName = MyTableName,
+                AttributeDefinitions =
+                {
+                    new AttributeDefinition
+                    {
+                        AttributeName = "Id",
+                        AttributeType = "N"
+                    },
+                    new AttributeDefinition
+                    {
+                        AttributeName = "Movie",
+                        AttributeType = "S"
+                    }
+                },
+                KeySchema =
+                {
+                    new KeySchemaElement
+                    {
+                        AttributeName = "Id",
+                        KeyType = "HASH" // Partition Key
+                    },
+                    new KeySchemaElement
+                    {
+                        AttributeName = "Movie",
+                        KeyType = "Range" // Sort Key
+                    }
+                },
+
+                BillingMode = BillingMode.PAY_PER_REQUEST
+            };
+
+            await client.CreateTableAsync(request, token);
+
+            DescribeTableResponse response;
+
+            do
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"Creating table {MyTableName}...");
+
+                response = await client.DescribeTableAsync(MyTableName, token);
+
+            } while (response.Table.TableStatus == TableStatus.CREATING);
+
+            if (response.Table.TableStatus == TableStatus.ACTIVE)
+            {
+                Console.WriteLine($"\nTable {MyTableName} was successfully created.");
+            }
+
+            Console.WriteLine("\nPress any key to continue...\n");
+            Console.ReadKey();
+        }
+
+
+        private static async Task DeleteTable(IAmazonDynamoDB client, CancellationToken token)
+        {
+            DeleteTableRequest request = new DeleteTableRequest(MyTableName);
+
+            await client.DeleteTableAsync(request, token);
+
+            DescribeTableResponse response;
+
+            string expectedExceptionMessage = 
+                $"Requested resource not found: Table: {MyTableName} not found";
+
+            try
+            {
+                do
+                {
+                    Thread.Sleep(2000);
+                    Console.WriteLine($"Deleting table {MyTableName}...");
+
+                    response = await client.DescribeTableAsync(MyTableName, token);
+
+                } while (response.Table.TableStatus == TableStatus.DELETING);
+            }
+            catch (ResourceNotFoundException e) when (CheckDeleteTableException(e, expectedExceptionMessage))
+            {
+                Console.WriteLine($"\nTable {MyTableName} was successfully deleted.");
+            }
+
+            Console.WriteLine("\nPress any key to continue...\n");
+            Console.ReadKey();
+        }
+
+
+        private static bool CheckDeleteTableException(Exception excp, string expectedExceptionMessage)
+        {
+            return excp.Message.Contains(expectedExceptionMessage) &&
+                   excp.Source == "AWSSDK.Core";
+        }
+    }
+}
---
+++ IAM/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Auth.AccessControlPolicy;
+using Amazon.IdentityManagement;
+using Amazon.IdentityManagement.Model;
+
+namespace IAM
+{
+    class Program
+    {
+        private const string SomeUser = "SomeUser";
+        private const string AnotherUser = "AnotherUser";
+
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Welcome To IAM!\n\n");
+
+            CancellationTokenSource source = new CancellationTokenSource();
+            CancellationToken token = source.Token;
+
+            using (IAmazonIdentityManagementService client =
+                new AmazonIdentityManagementServiceClient(RegionEndpoint.USEast1))
+            {
+                //await CreateIaMUsers(client, token);
+                //await AddUsersToAdminGroup(client, token);
+                await AttachAdminPolicyToAdminGroup(client, token);
+
+                Console.ReadKey();
+
+
+            }
+        }
+
+        private static async Task AttachAdminPolicyToAdminGroup(IAmazonIdentityManagementService client, 
+            CancellationToken token)
+        {
+            const string policyDocument =
+                @"
+                {
+                    ""Version"": ""2012-10-17"",
+                    ""Statement"": [
+                        {
+                            ""Effect"": ""Allow"",
+                            ""Action"": ""*"",
+                            ""Resource"": ""*""
+                        }
+                    ]
+                }";
+
+            CreatePolicyRequest request = new CreatePolicyRequest()
+            {
+                Description = "Policy for Administrators",
+                PolicyDocument = policyDocument,
+                PolicyName = "AllAccess"
+            };
+
+            CreatePolicyResponse response = await client.CreatePolicyAsync(request, token);
+            //throw new NotImplementedException();
+        }
+
+        private static async Task CreateIaMUsers(IAmazonIdentityManagementService client,
+            CancellationToken token)
+        {
+            CreateUserRequest request = new CreateUserRequest(SomeUser);
+            CreateUserResponse response = await client.CreateUserAsync(request, token);
+
+            Console.WriteLine($"The ARN of the new user is {response.User.Arn}");
+
+            request = new CreateUserRequest(AnotherUser);
+            response = await client.CreateUserAsync(request, token);
+
+            Console.WriteLine($"The ARN of the new user is {response.User.Arn}");
+        }
+
+
+        private static async Task AddUsersToAdminGroup(IAmazonIdentityManagementService client, 
+            CancellationToken token)
+        {
+            CreateGroupRequest groupRequest = new CreateGroupRequest("Admins");
+            CreateGroupResponse response = await client.CreateGroupAsync(groupRequest, token);
+
+            AddUserToGroupRequest request = new AddUserToGroupRequest("Admins", SomeUser);
+            var foo = await client.AddUserToGroupAsync(request, token);
+
+            request = new AddUserToGroupRequest("Admins", AnotherUser);
+            foo = await client.AddUserToGroupAsync(request, token);
+        }
+    }
+}
